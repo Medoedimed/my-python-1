dc6b5b315f520339cc5fee4067a1e2ade37f2f88
Medoedimed <maxtermit87@gmail.com>
2026-07-19 16:50:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ q = int(input(f'1-10\n'))
 for i in range(rn.randint(1, 5) * q):
   print('Hello, world')
   tm.sleep(0.1)
+tm.sleep(1)
+w = input(f'input\n')
+e = tm.time()
+while rn.random() < 0.9 and tm.time() - e < 3:
+  print(w)
+  tm.sleep(0.2)
+if tm.time() - e >= 3:
+  print('Good luck')
